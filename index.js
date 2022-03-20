66c2957b48217bb715ffc54ff829ab96134358e4
Luis <jqluis2011@gmail.com>
2022-03-20 15:20:16 -0700

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+
+
+const employee = {
+
+    name: "Luis",
+    streetAddress: "2011 Ave",
+};
+
+
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+
+    const newInfo = {...employee};
+
+    newInfo[key] = value;
+
+    return newInfo;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+
+   employee[key] = value;
+
+    
+return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+
+    const newInfo = {...employee};
+
+    delete newInfo[key];
+
+    return newInfo;
+}
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+
+    delete employee[key];
+    
+    return employee;
+}
